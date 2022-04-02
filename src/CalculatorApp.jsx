e1f3a7acb092a378624b301dcb807791089c1357
Chris Williams <chrisdw2110@gmail.com>
2022-04-02 16:03:54 +0100

    Tidied up files and added styled box

---
+++ src/CalculatorApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CalculatorApp.css'
+
+function CalculatorApp() {
+  return (
+    <div className='calcApp'>
+      This will be the calculator app block
+    </div>
+  )
+}
+
+export default CalculatorApp
