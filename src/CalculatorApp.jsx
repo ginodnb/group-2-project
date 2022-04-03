b638f002f5772e813370daba05c57d10a84d444d
Chris Williams <chrisdw2110@gmail.com>
2022-04-03 17:21:04 +0100

    Added the index.css back in and adjusted the style

--- src/CalculatorApp.jsx
+++ src/CalculatorApp.jsx
@@ -3,7 +3,7 @@ import './CalculatorApp.css'
 
 function CalculatorApp() {
   return (
-    <div className='calcApp'>
+    <div className='calcApp border-gradient border-gradient-purple'>
       This will be the calculator app block
     </div>
   )
