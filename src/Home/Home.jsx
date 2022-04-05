a7e6cde9627d74d4f4619a023094bd5171d1c1ad
ginodnb <ginoarnone90@gmail.com>
2022-04-05 19:24:12 +0100

    commiting p

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 import "./HomeStyle.css";
 
 function Home() {
+    const num = 2;
+    const calc = () => {
+        return ({num} * 39)
+    };
+    // console.log(calc);
     return (
     <div>
         Home
+        <label htmlFor="result"></label>
+        <span id='result'>{2 * {num}}</span>
+        <p>Hi</p>
+        
+        
+
+
     </div>
     )
 }
