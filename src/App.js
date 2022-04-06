1e6b8515a1b8b0408e253da8572346681889e6a9
ginodnb <ginoarnone90@gmail.com>
2022-04-06 16:52:00 +0100

    added home, added styling for home, multiplier is working, reset button is not

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Button } from 'primereact/button';
 
 function App() {
   return (
@@ -16,10 +18,13 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+          </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,22 +1,72 @@
-import React from 'react';
+import React, { useState, useForm } from 'react';
 import "./HomeStyle.css";
+import { Button } from 'primereact/button';
+import { InputNumber } from 'primereact/inputnumber';
+
 
 function Home() {
-    const num = 2;
-    const calc = () => {
-        return ({num} * 39)
-    };
+    const ops = ['*', '/'];
+
+    // const textInput = React.useRef();
+    // const clearInput = () => (textInput.current.value = 0);
+
+    // const {reset } = useForm<FormValues>({
+    //     defaultValues: {
+    //         numberOne: 0,
+    //         numberTwo: 0
+    //     }
+    // })
+
+    const [numberOne, setNumberOne] = useState();
+    const [numberTwo, setNumberTwo] = useState();
+    const [result, setResult] = useState(0);
+    // const [reset, setReset] = useState();
+
+    const multiply = () => {
+      setResult(numberOne * numberTwo)
+
+    }
+
+    const handleChange = (par1) => {
+        if (par1.id === 'num1') {
+        setNumberOne(Number(par1.value)) 
+        }     else {
+        setNumberTwo(Number(par1.value))
+        } 
+    
+    }
+
     // console.log(calc);
     return (
-    <div>
-        Home
-        <label htmlFor="result"></label>
-        <span id='result'>{2 * {num}}</span>
-        <p>Hi</p>
-        
-        
+    <div className='main'>
 
 
+        <div className='main-2'>
+        <div className='header-div'>
+        <h1>The Multiplier Machine</h1>
+        </div>
+
+        <div className='main-3'>
+
+        <p>Your number is {result}</p>
+
+            <input type='number' onChange={(e) => handleChange(e.target)} value={numberOne} id='num1' ></input>
+            <label   id='num1' htmlFor='num1'></label>
+            <input type='number' onChange={(e) => handleChange(e.target)} value={numberTwo} id='num2'></input>
+            <label  id='num2' htmlFor='num2' />
+
+            <Button id='multiply' onClick={multiply}>Multiply</Button>
+            <br />
+            <input type='button' value="Reset" id='restter'></input>
+            {/* <label htmlFor="resetter"></label> */}
+
+
+            
+
+        </div>
+    
+        </div>
+        
     </div>
     )
 }
